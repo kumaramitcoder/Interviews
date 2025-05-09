6b478f70f005efed87615c759f6f694b79554dc8
Amit Kumar <kumaramit.talk2u@gmail.com>
2025-05-10 00:24:26 +0530

    Test Commit

--- JavaInterview/src/array/Sum.java
+++ JavaInterview/src/array/Sum.java
@@ -3,5 +3,13 @@ package array;
 public class Sum {
     public static void main(String[] args) {
         System.out.println("hjhbsjhsdbf");
+        int a =19;
+        int b = 22;
+        Sum.SUm(a, b);
+    }
+
+    public static void SUm(int a, int b){
+        int sum = a+b;
+        System.out.println("dfghjk");
     }
 }
