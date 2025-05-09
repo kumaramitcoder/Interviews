9a2976b92c9e4d6fa3a7312a129d06d5421e44b1
amitsah <kumaramit.talk2u@gmail.com>
2025-05-10 00:52:09 +0530

    Substract method is added

--- JavaInterview/src/array/Sum.java
+++ JavaInterview/src/array/Sum.java
@@ -1,15 +1,24 @@
 package array;
 
+import java.util.Arrays;
+
 public class Sum {
     public static void main(String[] args) {
         System.out.println("hjhbsjhsdbf");
         int a =19;
         int b = 22;
         Sum.SUm(a, b);
+        Sum.SUbstract(a,b);
     }
 
     public static void SUm(int a, int b){
         int sum = a+b;
         System.out.println("dfghjk");
     }
+    public static void SUbstract(int a, int b){
+        int sum = a-b;
+        System.out.println("sub "+ sum);
+    }
+
+
 }
